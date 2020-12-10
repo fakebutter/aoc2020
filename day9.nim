14975840aaab950c3dd019dee75c1b09988c534b
fakebutter <cheeliang@fastmail.fm>
2020-12-11 00:06:49 +0800

    Simplify day 9

--- day9.nim
+++ day9.nim
@@ -5,32 +5,27 @@ import utils
 
 proc part1(nums: seq[int]): int =
   var history = initDeque[int]()
+  for i in nums[0..<25]:
+    history.addLast(i)
 
-  for num in nums:
-    if history.len < 25:
-      history.addLast(num)
-      continue
-    else:
-      var valid = false
-      for i in history:
-        let j = num - i
-        if i != j and j in history:
-          valid = true
+  for num in nums[25..^1]:
+    var valid = false
+    for i in history:
+      let j = num - i
+      if i != j and j in history:
+        valid = true
 
-      if not valid:
-        return num
+    if not valid:
+      return num
 
-    if history.len == 25:
-      history.popFirst()
+    history.popFirst()
     history.addLast(num)
 
 proc found(nums: seq[int], start: int, stop: int) =
-  var xs = newSeq[int]()
-  for i in start..stop:
-    xs.add(nums[i])
-  echo min(xs) + max(xs)
+  let sub = nums[start..stop]
+  echo min(sub) + max(sub)
 
-proc part2(nums: seq[int], target: int) =
+proc part2(nums: seq[int], target: int): (int, int) =
   var sums = newSeq[int]()
   for (i, num) in nums.pairs:
     if i == 0:
@@ -39,14 +34,13 @@ proc part2(nums: seq[int], target: int) =
       sums.add(sums[i - 1] + num)
 
     if sums[i] == target:
-      found(nums, 0, i)
-      return
+      return (0, i)
     for j in 0..<i:
       if sums[i] - sums[j] == target:
-        found(nums, j, i)
-        return
+        return (j, i)
 
 let nums = get_lines().map(parseInt)
 let p1 = part1(nums)
 echo p1
-part2(nums, p1)
+let (start, stop) = part2(nums, p1)
+found(nums, start, stop)
