3fba8a782e899126274b894108d38b34847fdb8f
fakebutter <cheeliang@fastmail.fm>
2020-12-10 23:59:34 +0800

    Simplify day 10

--- day10.nim
+++ day10.nim
@@ -6,60 +6,45 @@ import utils
 
 proc part1(jolts: seq[int]) =
   var jolts = sorted(jolts)
-
+  jolts.add(max(jolts) + 3)
   var
     diff1, diff3 = 0
+
   for (idx, jolt) in jolts.pairs:
-    var prev: int
-    if idx == 0:
-      prev = 0
+    let prev = if idx == 0:
+      0
     else:
-      prev = jolts[idx - 1]
+      jolts[idx - 1]
 
     if jolt - prev == 1:
       diff1 += 1
     else:
       diff3 += 1
 
-  let prev = jolts[^1]
-  let final = max(jolts) + 3
-  if final - prev == 1:
-    diff1 += 1
-  else:
-    diff3 += 1
-
   echo diff1 * diff3
 
 proc part2(jolts: seq[int]) =
   var jolts = sorted(jolts)
+  jolts.insert(0, 0)
   jolts.add(max(jolts) + 3)
+
   var total = jolts.map((_) => 0)
   total[^1] = 1
 
   var i = jolts.len - 2
   while i >= 0:
-    let cur = jolts[i]
     var subtotal = 0
+
     for j in i+1..<jolts.len:
-      let next = jolts[j]
-      if next - cur <= 3:
+      if jolts[j] - jolts[i] <= 3:
         subtotal += total[j]
       else:
         break
-    total[i] = subtotal
 
+    total[i] = subtotal
     i -= 1
 
-  var subtotal = 0
-  let cur = 0
-  for i in 0..<jolts.len:
-    let next = jolts[i]
-    if next - cur <= 3:
-      subtotal += total[i]
-    else:
-      break
-
-  echo subtotal
+  echo total[0]
 
 let jolts = get_lines().map(parseInt)
 part1(jolts)
