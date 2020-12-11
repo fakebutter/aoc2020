eaebe2f5792f638a208010ca51352ad9bb6d5499
fakebutter <cheeliang@fastmail.fm>
2020-12-11 21:14:25 +0800

    Tidy up day 11

--- day11.nim
+++ day11.nim
@@ -1,140 +1,150 @@
 import utils
-import strutils
 import sequtils
 import algorithm
 
 type
   Pair = tuple
     r, c: int
-  Map = seq[seq[char]]
+  Map = ref object
+    map: seq[seq[char]]
+    width, height: int
 
-proc parse_map(lines: seq[string]): Map =
-  lines.mapIt(toSeq(it.items))
+proc `+`(lhs: Pair, rhs: Pair): Pair =
+  var lhs = lhs
+  lhs.r += rhs.r
+  lhs.c += rhs.c
+  return lhs
 
-proc get_size(map: var seq[seq[char]]): Pair =
-  return (map.len, map[0].len)
+proc `[]`(map: Map, pos: Pair): char =
+  map.map[pos.r][pos.c]
 
-proc has_visible_occupied(map: var Map, pos: Pair, dir: Pair): bool =
-  let (height, width) = get_size(map)
-  var
-    r = pos[0]
-    c = pos[1]
+proc `[]=`(map: var Map, pos: Pair, seat: char) =
+  map.map[pos.r][pos.c] = seat
 
-  r += dir[0]; c += dir[1]
-  while r >= 0 and r < height and c >= 0 and c < width:
-    if map[r][c] == '#':
-      return true
-    elif map[r][c] == 'L':
-      return false
-    r += dir[0]; c += dir[1]
+proc count(map: Map, c: char): int =
+  map.map.mapIt(it.mapIt(if it == c: 1 else: 0)).concat.sum
 
-  return false
+proc valid(map: Map, pos: Pair): bool =
+  pos.r >= 0 and pos.r < map.height and pos.c >= 0 and pos.c < map.width
 
-proc get_adjacent_occupied(map: var Map, r: int, c: int): int =
-  let (height, width) = get_size(map)
-  proc valid(r: int, c: int): bool = r >= 0 and r < height and c >= 0 and c < width
+iterator iter(map: Map): (Pair, char) =
+  for (r, row) in map.map.pairs:
+    for (c, seat) in row.pairs:
+      yield ((r, c), seat)
 
-  for delta in [
-    (-1, -1), (-1, 0), (-1, 1),
-    (0, -1), (0, 1),
-    (1, -1), (1, 0), (1, 1),
-  ]:
-    if valid(r + delta[0], c + delta[1]):
-      if map[r + delta[0]][c + delta[1]] == '#':
-        result += 1
-
-proc get_visible_occupied(map: var seq[seq[char]], r: int, c: int): int =
-  for dir in [
-    (-1, -1), (-1, 0), (-1, 1),
-    (0, -1), (0, 1),
-    (1, -1), (1, 0), (1, 1)
-  ]:
-    if has_visible_occupied(map, (r, c), dir):
-      result += 1
-
-proc can_occupy(map: var seq[seq[char]], r: int, c: int): bool =
-  return get_adjacent_occupied(map, r, c) == 0
-
-proc can_vacate(map: var seq[seq[char]], r: int, c: int): bool =
-  return get_adjacent_occupied(map, r, c) >= 4
-
-proc can_occupy2(map: var seq[seq[char]], r: int, c: int): bool =
-  return get_visible_occupied(map, r, c) == 0
-
-proc can_vacate2(map: var seq[seq[char]], r: int, c: int): bool =
-  return get_visible_occupied(map, r, c) >= 5
-
-proc sig(map: var seq[seq[char]]): seq[Pair] =
-  for (r, row) in map.pairs:
+proc sig(map: var Map): seq[Pair] =
+  for (r, row) in map.map.pairs:
     for (c, seat) in row.pairs:
       if seat == '#':
         result.add((r, c))
   result.sort()
 
-proc step(map: var seq[seq[char]]): bool =
+proc newMap(lines: seq[string]): Map =
+  new(result)
+  result.map = lines.mapIt(toSeq(it.items))
+  result.height = result.map.len
+  result.width = result.map[0].len
+
+################################################################################
+
+# Part 1
+
+proc get_adjacent_occupied(map: var Map, pos: Pair): int =
+  for delta in [
+    (-1, -1), (-1, 0), (-1, 1),
+    (0, -1), (0, 1),
+    (1, -1), (1, 0), (1, 1),
+  ]:
+    let cur = pos + delta
+    if map.valid(cur) and map[cur] == '#':
+      result += 1
+
+proc can_occupy1(map: var Map, pos: Pair): bool =
+  return get_adjacent_occupied(map, pos) == 0
+
+proc can_vacate1(map: var Map, pos: Pair): bool =
+  return get_adjacent_occupied(map, pos) >= 4
+
+proc step1(map: var Map): bool =
   var
-    to_occupy = newSeq[Pair]()
-    to_vacate = newSeq[Pair]()
-  let sig_before = sig(map)
+    to_occupy, to_vacate = newSeq[Pair]()
+  let sig_before = map.sig
 
-  for (r, row) in map.pairs:
-    for (c, seat) in row.pairs:
-      if seat in ['#', 'L']:
-        if can_occupy(map, r, c):
-          to_occupy.add((r, c))
-        elif can_vacate(map, r, c):
-          to_vacate.add((r, c))
+  for (pos, seat) in map.iter:
+    if seat in ['#', 'L']:
+      if can_occupy1(map, pos):
+        to_occupy.add(pos)
+      elif can_vacate1(map, pos):
+        to_vacate.add(pos)
 
-  for (r, c) in to_occupy:
-    map[r][c] = '#'
-  for (r, c) in to_vacate:
-    map[r][c] = 'L'
+  for pos in to_occupy:
+    map[pos] = '#'
+  for pos in to_vacate:
+    map[pos] = 'L'
 
-  return sig_before != sig(map)
+  return sig_before != map.sig
 
-proc step2(map: var seq[seq[char]]): bool =
+################################################################################
+
+# Part 2
+
+proc has_visible_occupied(map: var Map, pos: Pair, dir: Pair): bool =
+  var pos = pos
+
+  pos = pos + dir
+  while map.valid(pos):
+    if map[pos] == '#':
+      return true
+    elif map[pos] == 'L':
+      return false
+    pos = pos + dir
+
+  return false
+
+proc get_visible_occupied(map: var Map, pos: Pair): int =
+  for dir in [
+    (-1, -1), (-1, 0), (-1, 1),
+    (0, -1), (0, 1),
+    (1, -1), (1, 0), (1, 1)
+  ]:
+    if has_visible_occupied(map, pos, dir):
+      result += 1
+
+proc can_occupy2(map: var Map, pos: Pair): bool =
+  return get_visible_occupied(map, pos) == 0
+
+proc can_vacate2(map: var Map, pos: Pair): bool =
+  return get_visible_occupied(map, pos) >= 5
+
+proc step2(map: var Map): bool =
   var
-    to_occupy = newSeq[Pair]()
-    to_vacate = newSeq[Pair]()
-  let sig_before = sig(map)
+    to_occupy, to_vacate = newSeq[Pair]()
+  let sig_before = map.sig
 
-  for (r, row) in map.pairs:
-    for (c, seat) in row.pairs:
-      if seat in ['#', 'L']:
-        if can_occupy2(map, r, c):
-          to_occupy.add((r, c))
-        elif can_vacate2(map, r, c):
-          to_vacate.add((r, c))
+  for (pos, seat) in map.iter:
+    if seat in ['#', 'L']:
+      if can_occupy2(map, pos):
+        to_occupy.add(pos)
+      elif can_vacate2(map, pos):
+        to_vacate.add(pos)
 
-  for (r, c) in to_occupy:
-    map[r][c] = '#'
-  for (r, c) in to_vacate:
-    map[r][c] = 'L'
+  for pos in to_occupy:
+    map[pos] = '#'
+  for pos in to_vacate:
+    map[pos] = 'L'
 
-  return sig_before != sig(map)
+  return sig_before != map.sig
 
-proc count(map: var seq[seq[char]], c: char): int =
-  for row in map:
-    for seat in row.items:
-      if seat == c:
-        result += 1
-
-proc print_map(map: var seq[seq[char]]) =
-  for row in map:
-    echo row.join("")
+################################################################################
 
 let lines = get_lines()
-var map = parse_map(lines)
-var dirty: bool
-while (dirty = step(map); dirty):
-  #echo count(map, '#')
-  #print_map(map)
-  discard
-echo count(map, '#')
 
-map = parse_map(lines)
-while (dirty = step2(map); dirty):
-  #echo count(map, '#')
-  #print_map(map)
+var map = newMap(lines)
+while step1(map):
   discard
-echo count(map, '#')
+echo map.count('#')
+
+map = newMap(lines)
+while step2(map):
+  discard
+echo map.count('#')
