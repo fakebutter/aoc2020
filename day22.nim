649596b98cf70bfdd9538e49fafa58ea47d35b05
fakebutter <cheeliang@fastmail.fm>
2020-12-29 18:49:52 +0800

    Tidy up day 22

--- day22.nim
+++ day22.nim
@@ -2,17 +2,18 @@ import deques
 import math
 import sequtils
 import strutils
-import sugar
 import utils
 
 proc parse_decks(lines: seq[string]): seq[Deque[int]] =
-  toSeq(lines.split((l) => l == ""))
-    .mapIt(it[1..^1].map(parseInt).toDeque)
+  lines.split
+    .mapIt(it[1..^1].map(parseInt))
+    .mapIt(it.toDeque)
 
 proc score_deck(deck: Deque[int]): int =
   for (i, c) in deck.pairs:
     result += (deck.len - i) * c
 
+# Unproven, but seems to work.
 let hash_deck = score_deck
 
 proc clone_decks(decks: seq[Deque[int]], size1: int, size2: int): seq[Deque[int]] =
@@ -21,7 +22,9 @@ proc clone_decks(decks: seq[Deque[int]], size1: int, size2: int): seq[Deque[int]
     toSeq(decks[1].items)[0..<size2].toDeque,
   ]
 
-proc part1(decks: var seq[Deque[int]]): int =
+proc part1(decks: seq[Deque[int]]): int =
+  var decks = decks
+
   while decks[0].len > 0 and decks[1].len > 0:
     let
       c1 = decks[0].popFirst
@@ -32,10 +35,13 @@ proc part1(decks: var seq[Deque[int]]): int =
     else:
       decks[1].addLast(c2); decks[1].addLast(c1)
 
-  return if decks[0].len > 0: 1 else: 2
+  let winner = if decks[0].len > 0: 1 else: 2
+  return score_deck(decks[winner - 1])
 
-proc part2(decks: var seq[Deque[int]]): int =
-  var history = newSeq[(int, int)]()
+proc part2(decks: seq[Deque[int]]): int =
+  var
+    decks = decks
+    history = newSeq[(int, int)]()
 
   while decks[0].len > 0 and decks[1].len > 0:
     let h = (hash_deck(decks[0]), hash_deck(decks[1]))
@@ -54,6 +60,7 @@ proc part2(decks: var seq[Deque[int]]): int =
       else:
         decks[1].addLast(c2); decks[1].addLast(c1)
     else:
+      # Recurse into sub-game.
       var sub_decks = clone_decks(decks, c1, c2)
       let sub_winner = part2(sub_decks)
 
@@ -62,14 +69,12 @@ proc part2(decks: var seq[Deque[int]]): int =
       else:
         decks[1].addLast(c2); decks[1].addLast(c1)
 
-  return if decks[0].len > 0: 1 else: 2
+  let winner = if decks[0].len > 0: 1 else: 2
+  return score_deck(decks[winner - 1])
 
-let lines = get_lines()
-var decks = parse_decks(lines)
+let
+  lines = get_lines()
+  decks = parse_decks(lines)
 
-var winner = part1(decks)
-echo score_deck(decks[winner - 1])
-
-decks = parse_decks(lines)
-winner = part2(decks)
-echo score_deck(decks[winner - 1])
+echo part1(decks)
+echo part2(decks)
