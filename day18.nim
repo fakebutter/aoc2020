0f34852b3ad3fbf5c0ea53153cba5b97aa6f243b
fakebutter <cheeliang@fastmail.fm>
2020-12-29 19:24:48 +0800

    Clean up day 18

--- day18.nim
+++ day18.nim
@@ -16,7 +16,7 @@ proc tokenize(line: string): seq[string] =
 ################################################################################
 # Part 1
 
-proc eval_top(stack: var seq[int], op: string): int =
+proc evalTop(stack: var seq[int], op: string): int =
   let
     rhs = stack.pop()
     lhs = stack.pop()
@@ -25,8 +25,8 @@ proc eval_top(stack: var seq[int], op: string): int =
     of "*": return lhs * rhs
     else: assert(false)
 
-proc eval_rd(tokens: var seq[string], stack: var seq[int]): int =
-  var cur_op = ""
+proc evalRd(tokens: var seq[string], stack: var seq[int]): int =
+  var curOp = ""
 
   # Some kind of weird shit recursive descent.
   while tokens.len > 0:
@@ -34,33 +34,33 @@ proc eval_rd(tokens: var seq[string], stack: var seq[int]): int =
     var added = false
 
     if token == "(":
-      stack.add(eval_rd(tokens, stack))
+      stack.add(evalRd(tokens, stack))
       added = true
     elif token == ")":
       return stack.pop()
     elif token in ["+", "*"]:
-      cur_op = token
+      curOp = token
     else:
       stack.add(parseInt(token))
       added = true
 
-    if added and cur_op != "":
-      stack.add(eval_top(stack, cur_op))
-      cur_op = ""
+    if added and curOp != "":
+      stack.add(evalTop(stack, curOp))
+      curOp = ""
 
   assert stack.len == 1
   return stack.pop()
 
-proc eval_eqn1(eqn: string): int =
+proc evalEqn1(eqn: string): int =
   var
     stack = newSeq[int]()
     tokens = reversed(tokenize(eqn))
-  return eval_rd(tokens, stack)
+  return evalRd(tokens, stack)
 
 ################################################################################
 # Part 2
 
-proc build_prn(tokens: var seq[string]): seq[string] =
+proc buildPrn(tokens: var seq[string]): seq[string] =
   var stack = newSeq[string]()
 
   # Shunting yard
@@ -85,7 +85,7 @@ proc build_prn(tokens: var seq[string]): seq[string] =
   while stack.len > 0:
     result.add(stack.pop())
 
-proc eval_prn(tokens: seq[string]): int =
+proc evalPrn(tokens: seq[string]): int =
   var stack = newSeq[string]()
   let popInt = () => parseInt(stack.pop())
 
@@ -98,12 +98,12 @@ proc eval_prn(tokens: seq[string]): int =
   assert stack.len == 1
   return parseInt(stack.pop())
 
-proc eval_eqn2(eqn: string): int =
+proc evalEqn2(eqn: string): int =
   var tokens = reversed(tokenize(eqn))
-  return eval_prn(build_prn(tokens))
+  return evalPrn(buildPrn(tokens))
 
 ################################################################################
 
-let lines = get_lines()
-echo lines.map(eval_eqn1).sum
-echo lines.map(eval_eqn2).sum
+let lines = getLines()
+echo lines.map(evalEqn1).sum
+echo lines.map(evalEqn2).sum
