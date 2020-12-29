023601ca1b389b27cfdd32c1e6e5b0ef28c6fc25
fakebutter <cheeliang@fastmail.fm>
2020-12-29 19:07:46 +0800

    Clean up day 21

--- day21.nim
+++ day21.nim
@@ -15,14 +15,14 @@ proc parse(lines: seq[string]): seq[Recipe] =
     if line =~ re"^(.*) \(contains (.*)\)$":
       result.add((matches[0].split(" "), matches[1].split(", ")))
 
-proc prune(candidates: TableRef[string, HashSet[string]], to_prune: seq[(string, string)]) =
+proc prune(candidates: TableRef[string, HashSet[string]], toPrune: seq[(string, string)]) =
   let
-    algs = to_prune.mapIt(it[0])
-    ings = to_prune.mapIt(it[1]).toHashSet
+    (allergens, tmp) = toPrune.unzip
+    ingredients = tmp.toHashSet
 
-  candidates.deleteKeys(algs)
-  for ingredients in candidates.mvalues:
-    ingredients.excl(ings)
+  candidates.del(allergens)
+  for ings in candidates.mvalues:
+    ings.excl(ingredients)
 
 proc solve(recipes: seq[Recipe]): Table[string, string] =
   var candidates = newTable[string, HashSet[string]]()
@@ -30,43 +30,43 @@ proc solve(recipes: seq[Recipe]): Table[string, string] =
   # Find common set of ingredients for each allergen.
   for (ingredients, allergens) in recipes:
     for allergen in allergens:
-      let ing_set = ingredients.toHashSet
-      candidates[allergen] = candidates.getOrDefault(allergen, ing_set) * ing_set
+      let ingSet = ingredients.toHashSet
+      candidates[allergen] = candidates.getOrDefault(allergen, ingSet) * ingSet
 
   while true:
-    var to_prune = newSeq[(string, string)]()
+    var toPrune = newSeq[(string, string)]()
 
     # Solve for allergens with only one candidate ingredient left.
     for (allergen, ingredients) in candidates.pairs:
       if ingredients.len == 1:
         let culprit = toSeq(ingredients.items)[0]
         result[allergen] = culprit
-        to_prune.add((allergen, culprit))
+        toPrune.add((allergen, culprit))
 
     # Remove what we have solved.
-    prune(candidates, to_prune)
-    if to_prune.len == 0:
+    prune(candidates, toPrune)
+    if toPrune.len == 0:
       break
 
 proc part1(recipes: seq[Recipe], solution: Table[string, string]): int =
   let
-    all_ingredients = recipes.mapIt(it[0]).concat
-    safe_ingredients = all_ingredients.toHashSet - toSeq(solution.values).toHashSet
+    allIngredients = recipes.mapIt(it[0]).concat
+    safeIngredients = allIngredients.toHashSet - toSeq(solution.values).toHashSet
 
   var freq = newCountTable[string]()
-  for ingredient in all_ingredients:
-    if ingredient in safe_ingredients:
+  for ingredient in allIngredients:
+    if ingredient in safeIngredients:
       freq.inc(ingredient)
   return toSeq(freq.values).sum
 
 proc part2(solution: Table[string, string]): string =
   return toSeq(solution.pairs)
-    .sorted(cmp_by_idx(0))
+    .sorted(cmpByIdx(0))
     .mapIt(it[1])
     .join(",")
 
 let
-  recipes = parse(get_lines())
+  recipes = parse(getLines())
   solution = solve(recipes)
 echo part1(recipes, solution)
 echo part2(solution)
